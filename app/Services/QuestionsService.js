cc2269ea2f33894b42fe298e9d3a470b60926c3b
Jacob <Jacobzeme8@gmail.com>
2023-02-06 16:41:57 -0700

    im okay with this

--- app/Services/QuestionsService.js
+++ app/Services/QuestionsService.js
@@ -15,15 +15,30 @@ class QuestionService{
   }
 
   checkAnswer(formData){
-    console.log(formData);
+    let answer = formData.answer.toString()
+    console.log(answer);
+    console.log(appState.jakeQuestion.correctAnswer); 
+    if(answer.toUpperCase() == appState.jakeQuestion.correctAnswer.toUpperCase()){
+      Pop.success("Correct!")
+      let index = appState.Questions.findIndex(q => q.correctAnswer == appState.jakeQuestion.correctAnswer)
+      appState.Questions.splice(index,1)
+      if(appState.Questions.length == 0){Pop.success("Congrats! you got all the questions corrext")
+        return}
+      this.drawQuestion()
+      
     
-    console.log(appState.jakeQuestion.correctAnswer.toUpperCase());
+    }
+    else{
+      Pop.error("wrong answer!")
+      return}
+      
+    
       
     }
   
 
   async getQuestions() {
-    const res = await trivia_api.get('?amount=10')
+    const res = await trivia_api.get('?amount=2')
     // console.log(res.data.results);
     const questionData = res.data.results
     appState.Questions = questionData.map(q => new Question(q))
